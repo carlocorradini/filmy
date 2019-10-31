6690e30da59af857bb2166a001a6710962efcc17
Carlo Corradini <carlo.ita98@gmail.com>
2019-10-31 18:11:45 +0100

    Better approach

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,17 @@
 import 'reflect-metadata';
-import path from 'path';
-import express from 'express';
-import favicon from 'serve-favicon';
 import { createConnection } from 'typeorm';
 import config from './config'; // ! Always first
 import logger from './logger';
-import routes from './routes';
-
-const app = express();
-
-app
-  .use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
-  .use('/static', express.static(path.join(__dirname, 'public')))
-  .use('/', routes);
+import createServer from './server';
 
 createConnection()
   .then(() => {
     logger.info('Database connected');
-
-    app
-      .listen(config.PORT, () => {
-        logger.info(`Server running on port ${config.PORT}`);
-      })
-      .on('error', (ex) => {
-        logger.error(`${ex.message}`);
-      });
+    return createServer(config.PORT);
+  })
+  .then((port) => {
+    logger.info(`Server running on port ${port}`);
   })
   .catch((ex) => {
-    logger.error(`${ex.toString()}`);
+    logger.error(ex.toString());
   });
---
+++ src/server/index.ts
@@ -0,0 +1,25 @@
+import path from 'path';
+import express from 'express';
+import favicon from 'serve-favicon';
+import routes from '../routes';
+
+const app = express();
+
+app
+  .use(favicon(path.join(__dirname, '../public', 'favicon.ico')))
+  .use('/static', express.static(path.join(__dirname, '../public')))
+  .use('/', routes);
+
+const createServer = (port: number) => {
+  return new Promise((resolve, reject) => {
+    app
+      .listen(port, () => {
+        resolve(port);
+      })
+      .on('error', (ex) => {
+        reject(ex.message);
+      });
+  });
+};
+
+export default createServer;
