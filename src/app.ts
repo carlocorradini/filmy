5b3fd63a46e87c38bcc13cdac21e725ac47d031c
Carlo Corradini <carlo.ita98@gmail.com>
2019-11-04 21:07:29 +0100

    test husky

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import { createServer } from './server';
 createConnection()
   .then(() => {
     logger.info('Database connected');
+    console.log('bubi');
     return createServer(config.PORT);
   })
   .then((port) => {
