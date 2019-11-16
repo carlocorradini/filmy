b6f3cb6fa500e087efa4483d9795cbc24a4e2c3a
Carlo Corradini <carlo.ita98@gmail.com>
2019-11-16 20:04:25 +0100

    Heroku testing

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,7 +8,7 @@ dotenv.config();
 // prettier-ignore
 const config: Config = {
   NODE_ENV: process.env.NODE_ENV !== undefined ? process.env.NODE_ENV : 'production',
-  PORT: process.env.PORT !== undefined ? parseInt(process.env.PORT, 10) : 443,
+  PORT: process.env.PORT !== undefined ? parseInt(process.env.PORT, 10) : 8080,
   DATABASE_TYPE: process.env.DATABASE_TYPE !== undefined ? process.env.DATABASE_TYPE : 'postgres',
   DATABASE_URL: process.env.DATABASE_URL !== undefined ? process.env.DATABASE_URL: 'postgres://username:password@localhost:5432/filmy',
   DATABASE_SSL: process.env.DATABASE_SSL !== undefined ? process.env.DATABASE_SSL === 'true' : true,
@@ -17,5 +17,4 @@ const config: Config = {
   DATABASE_MIGRATIONS: process.env.DATABASE_MIGRATIONS !== undefined ? process.env.DATABASE_MIGRATIONS : './database/migration/**/*.js',
   DATABASE_SUBSCRIBERS: process.env.DATABASE_SUBSCRIBERS !== undefined ? process.env.DATABASE_SUBSCRIBERS : './database/subscriber/**/*.js',
 };
-
 export default config;
