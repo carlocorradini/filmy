8f3a7de2d5b2d3843136aab72918b1ec738829fb
Ayoub Saghir <ayoub50.as@gmail.com>
2019-12-12 10:10:59 +0100

    db updated
    added data to the db

--- backup.sql
+++ backup.sql
@@ -1,4 +1,5 @@
 
+
 INSERT INTO public.actor (id, name, surname, gender, birth_date, death_date, profile, create_date, update_date) VALUES (1, 'Tom', 'Holland', 'M', '1996-06-01', NULL, 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/ip7aXVH8s6wXv8cY6KI14OZgCI8.jpg', '2019-11-27 14:46:03.931009', '2019-11-27 14:46:03.931009');
 INSERT INTO public.actor (id, name, surname, gender, birth_date, death_date, profile, create_date, update_date) VALUES (2, 'Jake', 'Gyllenhaal', 'M', '1980-12-19', NULL, 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/92sBuFC8tWPG7IqGDJNxysT7tIF.jpg', '2019-11-27 14:47:34.340676', '2019-11-27 14:47:34.340676');
 INSERT INTO public.actor (id, name, surname, gender, birth_date, death_date, profile, create_date, update_date) VALUES (3, 'Kristen', 'Bell', 'F', '1980-07-18', NULL, 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/9DoDVUkoXhT3O2R1RymPlOfUryl.jpg', '2019-11-27 14:52:04.99165', '2019-11-27 14:52:04.99165');
@@ -19,6 +20,10 @@ INSERT INTO public.actor (id, name, surname, gender, birth_date, death_date, pro
 INSERT INTO public.actor (id, name, surname, gender, birth_date, death_date, profile, create_date, update_date) VALUES (18, 'Will', 'Smith', 'M', '1968-09-25', NULL, 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/7XEzMCdMQffq8PrkLYjYb7QpIgW.jpg', '2019-11-27 15:27:20.386071', '2019-11-27 15:27:20.386071');
 INSERT INTO public.actor (id, name, surname, gender, birth_date, death_date, profile, create_date, update_date) VALUES (19, 'Jared', 'Leto', 'M', '1971-12-26', NULL, 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/i6zIpHiKBnaHtacJU6qgeoDjiw1.jpg', '2019-11-27 15:27:20.386071', '2019-11-27 15:27:20.386071');
 INSERT INTO public.actor (id, name, surname, gender, birth_date, death_date, profile, create_date, update_date) VALUES (20, 'Diane', 'Kruger', 'F', '1976-07-15', NULL, 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/cCVutSrGZg9kW2ux4hhvmm71ECJ.jpg', '2019-11-27 15:30:01.684989', '2019-11-27 15:30:01.684989');
+INSERT INTO public.actor (id, name, surname, gender, birth_date, death_date, profile, create_date, update_date) VALUES (21, 'Brad', 'Pitt', 'M', '1963-12-18', NULL, 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/kc3M04QQAuZ9woUvH3Ju5T7ZqG5.jpg', '2019-12-12 10:01:11.146971', '2019-12-12 10:01:11.146971');
+INSERT INTO public.actor (id, name, surname, gender, birth_date, death_date, profile, create_date, update_date) VALUES (22, 'Robin', 'Williams', 'M', '1951-07-21', '2014-08-11', 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/5KebSMXT8uj2D0gkaMFJ8VEp53.jpg', '2019-12-12 10:04:02.011146', '2019-12-12 10:04:02.011146');
+INSERT INTO public.actor (id, name, surname, gender, birth_date, death_date, profile, create_date, update_date) VALUES (23, 'Sylvester', 'Stallone', 'M', '1946-07-06', NULL, 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/4E6RSJNsiyhAD8XTq0S3Vwxn62x.jpg', '2019-12-12 10:07:05.206606', '2019-12-12 10:07:05.206606');
+
 
 INSERT INTO public.film (id, title, rating, release_date, poster, create_date, update_date) VALUES (1, 'Spider-Man: Far from Home', 76, '2019-06-26', 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/lcq8dVxeeOqHvvgcte707K0KVx5.jpg', '2019-11-27 14:43:59.609442', '2019-11-27 14:43:59.609442');
 INSERT INTO public.film (id, title, rating, release_date, poster, create_date, update_date) VALUES (2, 'Frozen II', 70, '2019-11-07', 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/qdfARIhgpgZOBh3vfNhWS4hmSo3.jpg', '2019-11-27 14:50:24.043626', '2019-11-27 14:50:24.043626');
@@ -31,7 +36,11 @@ INSERT INTO public.film (id, title, rating, release_date, poster, create_date, u
 INSERT INTO public.film (id, title, rating, release_date, poster, create_date, update_date) VALUES (9, 'Limitless', 72, '2011-03-08', 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/bQfOFEeXxi51ijQrbQY9qvMtYhZ.jpg', '2019-11-27 15:20:26.138413', '2019-11-27 15:20:26.138413');
 INSERT INTO public.film (id, title, rating, release_date, poster, create_date, update_date) VALUES (10, 'Suicide Squad ', 59, '2016-08-01', 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/e1mjopzAS2KNsvpbpahQ1a6SkSn.jpg', '2019-11-27 15:24:15.29219', '2019-11-27 15:24:15.29219');
 INSERT INTO public.film (id, title, rating, release_date, poster, create_date, update_date) VALUES (11, 'Mr. Nobody', 79, '2011-12-20', 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/jxd2XSsnUHHUbo50nzVnVnHRPVJ.jpg', '2019-11-27 15:28:58.965058', '2019-11-27 15:28:58.965058');
-
+INSERT INTO public.film (id, title, rating, release_date, poster, create_date, update_date) VALUES (12, 'Jumanji - The Next Level', 68, '2019-12-13', 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/8QKhGhDgjUzK4Nt1c9uYmOTLe8c.jpg', '2019-12-12 09:55:29.231143', '2019-12-12 09:55:29.231143');
+INSERT INTO public.film (id, title, rating, release_date, poster, create_date, update_date) VALUES (13, 'The Irishman', 80, '2019-11-01', 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/2Qi4m1w57701OXeNhu1wQwLKMY0.jpg', '2019-12-12 09:57:31.716522', '2019-12-12 09:57:31.716522');
+INSERT INTO public.film (id, title, rating, release_date, poster, create_date, update_date) VALUES (14, 'Ad astra: missione classificata', 61, '2019-09-20', 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/yUJJzKiAi5akgRYVJVw6nIEYTOi.jpg', '2019-12-12 09:59:44.239545', '2019-12-12 09:59:44.239545');
+INSERT INTO public.film (id, title, rating, release_date, poster, create_date, update_date) VALUES (15, 'Jumanji', 72, '1995-12-15', 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/p67m5dzwyxWd46a6of2c9IVfQz7.jpg', '2019-12-12 10:02:43.317176', '2019-12-12 10:02:43.317176');
+INSERT INTO public.film (id, title, rating, release_date, poster, create_date, update_date) VALUES (16, 'Rambo: Last Blood', 59, '2019-09-20', 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/1sElVrhcZfxdngXsBzSu6yZW1mM.jpg\', '2019-12-12 10:05:40.223401', '2019-12-12 10:05:40.223401');
 
 INSERT INTO public.film_actor (film_id, actor_id) VALUES (1, 1);
 INSERT INTO public.film_actor (film_id, actor_id) VALUES (1, 2);
@@ -56,7 +65,11 @@ INSERT INTO public.film_actor (film_id, actor_id) VALUES (10, 18);
 INSERT INTO public.film_actor (film_id, actor_id) VALUES (10, 19);
 INSERT INTO public.film_actor (film_id, actor_id) VALUES (11, 19);
 INSERT INTO public.film_actor (film_id, actor_id) VALUES (11, 20);
-
+INSERT INTO public.film_actor (film_id, actor_id) VALUES (12, 12);
+INSERT INTO public.film_actor (film_id, actor_id) VALUES (13, 6);
+INSERT INTO public.film_actor (film_id, actor_id) VALUES (14, 21);
+INSERT INTO public.film_actor (film_id, actor_id) VALUES (15, 22);
+INSERT INTO public.film_actor (film_id, actor_id) VALUES (16, 23);
 
 INSERT INTO public."user" (id, name, surname, username, email, password, create_date, update_date) VALUES ('4c4d470d-59e6-4504-86bd-1623019b511b', 'Ayoub', 'Saghir', 'ayoubsaghir', 'ayoub.saghir@studenti.unitn.it', '$2a$10$ydbURikO65LsMUyfp14hqOZvQQPm.nlO3.UZ5A5myKt7ptVYX9lFy', '2019-11-27 14:40:30.164042', '2019-11-27 14:40:30.164042');
 INSERT INTO public."user" (id, name, surname, username, email, password, create_date, update_date) VALUES ('b518546a-dc13-4919-aa22-897985833005', 'Luca', 'Santoro', 'lucasantoro', 'luca.santoro-1@studenti.unitn.it', '$2a$10$ydbURikO65LsMUyfp14hqOZvQQPm.nlO3.UZ5A5myKt7ptVYX9lFy', '2019-11-27 14:40:30.164042', '2019-11-27 14:40:30.164042');
@@ -65,4 +78,5 @@ INSERT INTO public."user" (id, name, surname, username, email, password, create_
 INSERT INTO public."user" (id, name, surname, username, email, password, create_date, update_date) VALUES ('8b975e22-2873-4168-868c-bd2cd4118630', 'Grigore', 'Andronic', 'grigoreandronic', 'grigore.andronic@studenti.unitn.it', '$2a$10$ydbURikO65LsMUyfp14hqOZvQQPm.nlO3.UZ5A5myKt7ptVYX9lFy', '2019-11-27 14:40:30.164042', '2019-11-27 14:40:30.164042');
 INSERT INTO public."user" (id, name, surname, username, email, password, create_date, update_date) VALUES ('dba906b7-8866-498c-96f4-c93583161961', 'Jorge', 'Ramirez', 'jorgedanielramirezmedina', 'jorge.ramirezmedina@unitn.it', '$2a$10$ydbURikO65LsMUyfp14hqOZvQQPm.nlO3.UZ5A5myKt7ptVYX9lFy', '2019-12-10 15:26:42.841683', '2019-12-10 15:26:42.841683');
 INSERT INTO public."user" (id, name, surname, username, email, password, create_date, update_date) VALUES ('1da19faa-3271-4aed-9b27-2fc2ffe46a3c', 'Renata', 'Guizzardi', 'renatasilvasouzaguizzardi', 'renata.guizzardi@unitn.it', '$2a$10$ydbURikO65LsMUyfp14hqOZvQQPm.nlO3.UZ5A5myKt7ptVYX9lFy', '2019-12-10 15:26:42.841683', '2019-12-10 15:26:42.841683');
+
 
